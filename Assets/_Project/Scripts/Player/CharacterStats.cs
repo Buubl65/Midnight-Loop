938048af2ac82fb1d5f4cb44882ec764c6eab2bd
Dmitriy Sherbak <dima.sherbak12@gmail.com>
2026-06-09 14:41:23 +0300

    fix

--- Assets/_Project/Scripts/Player/CharacterStats.cs
+++ Assets/_Project/Scripts/Player/CharacterStats.cs
@@ -24,6 +24,8 @@ public class CharacterStats : MonoBehaviour
     public float dashStaminaCost = 25f;
     public float jumpStaminaCost = 15f;
 
+
+
     [Header("Money")]
     public int money = 0;
 
