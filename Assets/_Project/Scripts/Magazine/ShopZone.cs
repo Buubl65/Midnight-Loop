031fc5af8350b9974c21875e41b44aefb1bf4bfa
Dmitriy Sherbak <dima.sherbak12@gmail.com>
2026-06-09 19:29:51 +0300

    Worked shi

--- Assets/_Project/Scripts/Magazine/ShopZone.cs
+++ Assets/_Project/Scripts/Magazine/ShopZone.cs
@@ -2,16 +2,37 @@ using UnityEngine;
 
 public class ShopZone : MonoBehaviour
 {
-    public GameObject shopUI;
+    private GameObject shopUI;
 
     void Start()
     {
-        shopUI.SetActive(false);
+        // Спочатку шукаємо активний Canvas
+        GameObject canvas = GameObject.Find("Canvas");
+
+        if (canvas != null)
+        {
+            // transform.Find вміє знаходити навіть ВИМКНЕНІ дочірні об'єкти!
+            Transform shopTransform = canvas.transform.Find("ShopUI");
+
+            if (shopTransform != null)
+            {
+                shopUI = shopTransform.gameObject;
+                shopUI.SetActive(false); // Про всяк випадок вимикаємо кодом
+            }
+            else
+            {
+                Debug.LogError("Не вдалося знайти 'ShopUI' всередині Canvas! Перевір точність назви.");
+            }
+        }
+        else
+        {
+            Debug.LogError("Головний об'єкт 'Canvas' не знайдено в сцені!");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && shopUI != null)
         {
             shopUI.SetActive(true);
 
